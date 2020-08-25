18c926d1f18299cd2abf66476db0806f855291e6
CASSCODE92 <cassie2012abioye@gmail.com>
2020-08-24 21:04:23 -0400

    description

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom';
 
+const pawtastic= (
+    <h3>Pawtastic</h3>
+    <span class="checkboxes">
+    <ul>
+        <input type= "checkbox" id="Human profile" class="checkbox" />
+        <label for="Human profile" class="switch"></label>
+
+        <input type= "checkbox" id="Pet basics" class="checkbox" />
+        <label for="Pet basics" class="switch"></label>
+
+        <input type= "checkbox" id="Pet details" class="checkbox" />
+        <label for="Pet details" class="switch"></label>
+
+        <input type= "checkbox" id="Confirm" class="checkbox" />
+        <label for="Confirm" class="switch"></label>
+    </ul>
+</span>
+
+<h6>Yay, we love dogs! Give us the basics about your pup.</h6>
+<form class="input">
+    <label name= "petsName">Name</label>
+
+    <label name= "firstName">Pet's Name</label> 
+    
+</form>
+)
