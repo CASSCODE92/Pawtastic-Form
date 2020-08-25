7f788ab8d5f745bc3e47030279c664a1e373db0d
CASSCODE92 <cassie2012abioye@gmail.com>
2020-08-24 21:20:29 -0400

    Girl Gender

--- app.js
+++ app.js
@@ -30,7 +30,10 @@ const pawtastic= (
     <p>Gender</p>
     <input type="radio" id="male" name="gender" value="male">
         <label for="male">Male</label><br>
-        
+        <input type="radio" id= "female"
+        name="gender" value="female">
+            <label for="female">Female</label>
+
     
 </form>
 )
